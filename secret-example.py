cc300c4f5962a95591f5edf724d7727f5f2d8f9d
Bas Nijholt <bas@nijho.lt>
2020-06-06 20:08:11 +0200

    add comments

--- secret-example.py
+++ secret-example.py
@@ -1,2 +1,6 @@
+# Change the variables and rename this file to secret.py
+
+# add your url here
 url = "https://home-assistant.duckdns.org"
+# get a "Long-Lived Access Token" at YOUR_URL/profile
 token = "AJKSDHHASJKDHA871263291873KHGSDKAJSGD"
