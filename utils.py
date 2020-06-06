081b789a735d7e2614aa45977ba471f4b1e3b3e8
Bas Nijholt <bas@nijho.lt>
2020-06-06 20:32:30 +0200

    add service function

--- utils.py
+++ utils.py
@@ -1,7 +1,7 @@
 import json
 from pathlib import Path
 
-from requests import get
+from requests import get, post
 
 from secret import url, token
 
@@ -16,3 +16,13 @@ this_folder = Path(__file__).resolve().parent
 def entity(entity_id):
     response_raw = get(f"{url}/api/states/{entity_id}", headers=headers)
     return json.loads(response_raw.text)
+
+
+def service(domain, service, entity_id, **data):
+    data = dict(data, entity_id=entity_id)
+    response_raw = post(
+        f"{url}/api/services/{domain}/{service}",
+        headers=headers,
+        data=json.dumps(data),
+    )
+    return response_raw
