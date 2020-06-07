4b54cbf7d86566396f440ce5edfb4f073c93a30f
Bas Nijholt <bas@nijho.lt>
2020-06-07 15:51:47 +0200

    get the unit from HA

--- widgets/power-usage.py
+++ widgets/power-usage.py
@@ -2,9 +2,11 @@ import json
 
 from utils import entity, icon_folder
 
-state = entity("sensor.power_consumption")["state"].replace("_", " ")
+info = entity("sensor.power_consumption")
+state = info["state"].replace("_", " ")
+unit = info["attributes"].get("unit_of_measurement", "")
 icon = str(
     icon_folder / "fa-flash.png"
 )  # from https://github.com/encharm/Font-Awesome-SVG-PNG
-info = dict(text=f"{state} kWh", icon_path=icon)
+info = dict(text=f"{state} {unit}", icon_path=icon)
 print(json.dumps(info))
