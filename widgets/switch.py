fe98189772856be9f37dab25e7eca89eee078ba6
Bas Nijholt <bas@nijho.lt>
2020-06-07 15:39:49 +0200

    switch.py can now do everything input_boolean.py could

--- widgets/switch.py
+++ widgets/switch.py
@@ -5,6 +5,7 @@ from utils import entity, icon_folder, service
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--entity_id", action="store", required=True)
+parser.add_argument("--icon", action="store")
 group = parser.add_mutually_exclusive_group(required=True)
 group.add_argument("--state", action="store_true")
 group.add_argument("--toggle", action="store_true")
@@ -12,16 +13,17 @@ args = parser.parse_args()
 
 if args.state:
     state = entity(args.entity_id)["state"]
-    icon = str(
-        icon_folder / "fa-lightbulb-o.png"
-    )  # from https://github.com/encharm/Font-Awesome-SVG-PNG
-
     info = dict(
         text=state,
-        icon_path=icon,
         background_color="255,255,0,255" if state == "on" else "75,75,75,255",
     )
+    if args.icon:
+        # download icons from https://github.com/encharm/Font-Awesome-SVG-PNG
+        # and place them in `../icons/`
+        info["icon"] = str(icon_folder / args.icon)
+
     print(json.dumps(info))
 elif args.toggle:
-    response = service("light", "toggle", args.entity_id)
+    domain = args.entity_id.split(".")[0]
+    response = service(domain, "toggle", args.entity_id)
     print("Toggling")
