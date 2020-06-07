f3e6b9b5022ed9157ec489f09483dfa99e0e5eef
Bas Nijholt <bas@nijho.lt>
2020-06-07 16:57:06 +0200

    remove comma

--- widgets/marcella-home.py
+++ widgets/marcella-home.py
@@ -3,5 +3,5 @@ import json
 from utils import entity, icon_folder
 
 state = entity("person.marcella")["state"].replace("_", " ")
-info = dict(text=f"is {state}", icon_path=str(icon_folder / "marcella.jpg"),)
+info = dict(text=f"is {state}", icon_path=str(icon_folder / "marcella.jpg"))
 print(json.dumps(info))
