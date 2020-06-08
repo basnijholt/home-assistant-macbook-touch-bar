d0dbf61fa86a2d445da6fef70c7b7c87d9cd98c4
Bas Nijholt <basnijholt@gmail.com>
2020-06-08 12:20:38 +0200

    add note about /

--- widgets/secret-example.py
+++ widgets/secret-example.py
@@ -1,6 +1,6 @@
 # Change the variables and rename this file to secret.py
 
-# add your url here
+# add your url here (without trailing / at the end!)
 url = "https://home-assistant.duckdns.org"
 # get a "Long-Lived Access Token" at YOUR_URL/profile
 token = "AJKSDHHASJKDHA871263291873KHGSDKAJSGD"
